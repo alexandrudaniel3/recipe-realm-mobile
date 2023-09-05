39fe2e7ccdbebb43e7ac14e15c8a26a801a8ca7a
alexandrudaniel3 <alexandrudanielbortos@gmail.com>
2023-09-05 14:39:21 +0300

    unit conversion styling

--- components/RecipeConverter.jsx
+++ components/RecipeConverter.jsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
 import { units, ingredientsDensity } from "../utils/conversionData";
-import { View, Text, TextInput, Pressable, Image } from "react-native";
-import convertIcon from "../assets/convert-icon.png";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import { convertData } from "../utils/conversionData";
 
-function ValueBox({value, setValue}) {
+function ValueBox({ value, setValue }) {
 
   const handleChange = (value) => {
     setValue(value);
-  }
+  };
 
   return (
-    <View>
+    <View style={{ alignItems: "center" }}>
       <TextInput
         onChangeText={handleChange}
         value={value}
         placeholder="Insert value here..."
         keyboardType="numeric"
+        style={styles.textInput}
+        placeholderTextColor={"#6E449CFF"}
       />
     </View>
-  )
+  );
 }
 
 export default function RecipeConverter() {
@@ -31,7 +32,7 @@ export default function RecipeConverter() {
   const [ingredientDensity, setIngredientDensity] = useState(1.00);
   const [ingredientOpen, setIngredientOpen] = useState(false);
   const [value, setValue] = useState(0);
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
 
   const closeOtherPickers = (currentPicker) => {
     if (currentPicker !== "from") setFromOpen(false);
@@ -41,17 +42,27 @@ export default function RecipeConverter() {
 
   function handleSubmit() {
     if (value) {
-      const test = convertData(fromUnit, toUnit, value, ingredientDensity);
-      setResult(value + fromUnit.substring(2) + ' = ' + test + toUnit.substring(2));
+      let test = convertData(fromUnit, toUnit, value, ingredientDensity);
+      if (typeof test === "number") {
+        test = test.toFixed(3);
+      }
+      setResult(value + fromUnit.substring(2) + " = " + test + toUnit.substring(2));
+    } else {
+      setResult("");
     }
   }
+
+  useEffect(() => {
+    handleSubmit();
+  }, [fromUnit, toUnit, value]);
 
   const DisplayIngredientPicker = () => {
     if ((fromUnit.includes("m:") && toUnit.includes("v:")) || (fromUnit.includes("v:") && toUnit.includes("m:"))) {
       return (
-        <View style={{zIndex: 1000}}>
-          <Text>In order to convert mass to volume and vice versa, please select an ingredient.</Text>
-          <DropDownPicker setValue={setIngredientDensity} value={ingredientDensity} items={ingredientsDensity} open={ingredientOpen}
+        <View style={{ zIndex: 1000 }}>
+          <Text style={styles.volumeWarning}>In order to convert mass to volume and vice versa, please select an ingredient:</Text>
+          <DropDownPicker setValue={setIngredientDensity} value={ingredientDensity} items={ingredientsDensity}
+                          open={ingredientOpen}
                           setOpen={(open) => {
                             setIngredientOpen(open);
                             if (open) {
@@ -59,7 +70,7 @@ export default function RecipeConverter() {
                             }
                           }}
                           zIndex={1000} zIndexInverse={2000} itemKey="label"
-                          showTickIcon={false} />
+                          showTickIcon={false} style={styles.selector} />
         </View>
       );
     } else {
@@ -69,31 +80,91 @@ export default function RecipeConverter() {
   };
 
   return (
-    <View>
+    <View style={{ marginHorizontal: 10 }}>
+      <Text style={styles.fromTo}>From:</Text>
       <DropDownPicker setValue={setFromUnit} value={fromUnit} items={units} open={fromOpen} setOpen={(open) => {
         setFromOpen(open);
         if (open) {
           closeOtherPickers("from");
         }
       }}
-                      zIndex={3000} zIndexInverse={1000} showTickIcon={false} />
+                      zIndex={3000} zIndexInverse={1000} showTickIcon={false}
+                      style={styles.selector} />
+      <Text style={styles.fromTo}>To:</Text>
       <DropDownPicker setValue={setToUnit} value={toUnit} items={units} open={toOpen} setOpen={(open) => {
         setToOpen(open);
         if (open) {
           closeOtherPickers("to");
         }
       }}
-                      zIndex={2000} zIndexInverse={2000} showTickIcon={false} />
+                      zIndex={2000} zIndexInverse={2000} showTickIcon={false}
+                      style={styles.selector} />
       <DisplayIngredientPicker />
-      <ValueBox setValue={setValue}/>
-      <View style={{zIndex: 100}}>
-        <Pressable onPress={handleSubmit} style={{ width: 30, margin: 5 }}>
-          <Image source={convertIcon} style={{ width: "90%", height: undefined, aspectRatio: 1, tintColor: "#6E449CFF" }} />
-        </Pressable>
+      <ValueBox setValue={setValue} />
+      <View style={{ zIndex: 100 }}>
+        {/*<Pressable onPress={handleSubmit} style={styles.convertButtonContainer}>*/}
+        {/*  <Text style={styles.convertButton}>Convert</Text>*/}
+        {/*</Pressable>*/}
       </View>
       {result ?
-        <Text>{result}</Text> :
+        <Text style={styles.resultText}>{result}</Text> :
         null}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    selector: {
+      borderStyle: "solid",
+      borderColor: "#6E449CFF",
+      borderWidth: 2,
+      marginVertical: 5,
+      width: "80%",
+      alignSelf: "center",
+    },
+    fromTo: {
+      width: "75%",
+      alignSelf: "center",
+      color: "#6E449CFF",
+    },
+  volumeWarning: {
+    color: "#6E449CFF",
+    textAlign: "center",
+    fontSize: 20,
+  },
+    textInput: {
+      borderStyle: "solid",
+      borderColor: "#6E449CFF",
+      borderWidth: 2,
+      margin: 20,
+      borderRadius: 7,
+      width: "60%",
+      height: 50,
+      padding: 10,
+      color: "#6E449CFF",
+    },
+    convertButtonContainer: {
+      alignItems: "center",
+    },
+    convertButton: {
+      alignSelf: "center",
+      padding: 5,
+      borderStyle: "solid",
+      borderColor: "#6E449CFF",
+      borderWidth: 2,
+      backgroundColor: "#6E449CFF",
+      overflow: "hidden",
+      borderRadius: 7,
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    resultText: {
+      alignSelf: "center",
+      margin: 10,
+      fontWeight: "bold",
+      fontSize: 30,
+      color: "#6E449CFF",
+    },
+  },
+);
--- pages/UnitConverter.jsx
+++ pages/UnitConverter.jsx
@@ -1,31 +1,12 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import RecipeConverter from "../components/RecipeConverter";
 
 export default function UnitConverter() {
 
-  const [page, setPage] = useState('recipe');
-
-  const UnitConverterContent = () => {
-    switch (page) {
-      case 'recipe':
-        return <RecipeConverter />;
-      case 'butter':
-        return <Text>butter</Text>;
-      case 'temperature':
-        return <Text>temperature</Text>;
-    }
-  }
-
   return (
     <View>
       <Text style={styles.unitConverterHeader}>Unit Converter</Text>
-      <View style={{flexDirection: "row"}}>
-        <Pressable onPress={() => setPage('recipe')}><Text>Common</Text></Pressable>
-        <Pressable onPress={() => setPage('butter')}><Text>Butter</Text></Pressable>
-        <Pressable onPress={() => setPage('temperature')}><Text>Temperature</Text></Pressable>
-      </View>
-      <UnitConverterContent />
+      <RecipeConverter />
     </View>
   )
 }
